0c332d3a805cafd1f441cd3b3a7b82e00cbd2bc2
BagiRil <90069837+BagiRil@users.noreply.github.com>
2021-11-19 22:51:02 +0100

    Update h.lua

--- h.lua
+++ h.lua
@@ -1,18 +1,2 @@
-local library = {}
-local MainModule = Game:GetObjects("rbxassetid://8043749563")[1]
-
-local function GetModule(Module)
-	return loadstring(MainModule[Module].Source)()
-end
-
-library.Get = {
-	Utils = GetModule("Utils"),
-	Window = GetModule("Window"),
-	Tab = GetModule("Tab")
-}
-
-function library:Window(Options)
-	library.Get.Window:Window(Options)
-end
-
+local library = require(8043967622)
 return library
