3f158bc9f94c829ab3403e576dad4a8e35322773
BagiRil <90069837+BagiRil@users.noreply.github.com>
2021-11-20 11:48:05 +0100

    Update h.lua

--- h.lua
+++ h.lua
@@ -1,3 +1,22 @@
-local Library = loadstring(Game:GetObjects("rbxassetid://8043967622")[1].Source)()
+local MainModule = Game:GetObjects("rbxassetid://7974127463")[1]
 
-Library:LOL()
+local function GetModule(Module)
+	return loadstring(MainModule[Module].Source)()
+end
+local library = {}
+
+local Get = {
+	Utils = GetModule("Utils"),
+	Window = GetModule("Window")
+}
+
+function library:LOL()
+	print(" Oke Oke Oke ")
+end
+
+function library:Window(Options)
+	return Get.Window:New(Options)
+end
+
+
+return library
